f711facc37a65e65e4603c0ca00f3dd6cddc84c9
SirhcMods <sirhcmods@gmail.com>
2026-01-11 01:35:47 -0500

    Temporarily disable normals_split_custom_set_from_vertices
    This can crash blender if the mesh being imported is too high poly or if theres too much data being given at once. Will probably readd with better logic

--- umodel_tools/third_party/io_import_scene_unreal_psa_psk_280.py
+++ umodel_tools/third_party/io_import_scene_unreal_psa_psk_280.py
@@ -1049,7 +1049,7 @@ def pskimport(filepath,
 
         if Normals is not None:
             mesh_data.polygons.foreach_set("use_smooth", [True] * len(mesh_data.polygons))
-            mesh_data.normals_split_custom_set_from_vertices(Normals)
+            #mesh_data.normals_split_custom_set_from_vertices(Normals)
             #mesh_data.use_auto_smooth = True
 
     #===================================================================================================
